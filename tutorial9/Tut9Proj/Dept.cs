c17876033a3ca84ada045a25b80e4cc8a3a3c879
s18827 <s18827@pjwstk.edu.pl>
2020-05-08 23:11:46 +0200

    Tut9Proj: Only task11 left to do

---
+++ tutorial9/Tut9Proj/Dept.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tut9Proj
+{
+    public class Dept
+    {
+        public int Deptno { get; set; }
+        public string Dname { get; set; }
+        public string Loc { get; set; }
+    }
+}
---
+++ tutorial9/Tut9Proj/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Threading.Tasks;
+using System;
+
+namespace Tut9Proj
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var d = new LinqSamples();
+            // checking solutions:
+            d.Task1();
+            d.Task2();
+            d.Task3();
+            d.Task4();
+            d.Task5();
+            d.Task6();
+            d.Task7();
+            d.Task8();
+            d.Task9();
+            d.Task10();
+            d.Task11();
+            d.Task12();
+        }
+    }
+}
